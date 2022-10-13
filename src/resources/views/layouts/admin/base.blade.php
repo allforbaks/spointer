cb30f8b663a0a75a3bf0526fa1197f76c20e9895
AndreyP0 <andriy.polyak.1997@gmail.com>
2022-10-13 19:35:01 +0300

    created migration

--- src/resources/views/layouts/admin/base.blade.php
+++ src/resources/views/layouts/admin/base.blade.php
@@ -16,11 +16,14 @@
         <!-- Main Sidebar Container -->
         <aside class="main-sidebar sidebar-dark-primary elevation-4">
             <!-- Brand Logo -->
-            <a href="index3.html" class="brand-link">
+            <a href={{ route('admin.index') }} class="brand-link">
                 <span class="brand-text font-weight-light">Admin Panel</span>
             </a>
             <!-- Sidebar Menu -->
             <nav class="mt-2">
+                <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+                    <li><a href={{ route('admin.posts.index') }} class="nav-link">Posts</a></li>
+                </ul>
             </nav>
             <!-- /.sidebar-menu -->
     </div>
@@ -40,7 +43,6 @@
                         <ol class="breadcrumb float-sm-right">
                             <li class="breadcrumb-item"><a href="#">Home</a></li>
                             <li class="breadcrumb-item active">Dashboard v1</li>
-                            <button class="btn btn-danger">asdas</button>
                         </ol>
                     </div><!-- /.col -->
                 </div><!-- /.row -->
--- src/routes/web.php
+++ src/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
-use App\Http\Controllers\Admin\Post\IndexController;
+use App\Http\Controllers\Admin\PostController;
+use App\Http\Controllers\Admin\AdminController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -21,7 +22,8 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::prefix('admin')->group(function () {
-    Route::get('/posts', function () {
-        return view('admin.posts.index');
-    })->name('admin.posts.index');
+    Route::get('/', [AdminController::class, 'index'])->name('admin.index');
+    Route::prefix('posts')->group(function () {
+        Route::get('/', [PostController::class, 'index'])->name('admin.posts.index');
+    });
 });
