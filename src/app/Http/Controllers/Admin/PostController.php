6fb20c975b7ecf4c9f7aa966fd3e84159fbe7496
AndreyP0 <andriy.polyak.1997@gmail.com>
2022-10-23 20:06:36 +0300

    restart

--- src/app/Http/Controllers/Admin/PostController.php
+++ src/app/Http/Controllers/Admin/PostController.php
@@ -3,12 +3,16 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Category;
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
     public function index()
     {
-        return view('admin.posts.index');
+        $posts = Post::paginate(10);
+        $categories = Category::all();
+        return view('admin.posts.index', compact('posts', 'categories'));
     }
 }
